9213e569f465587b2178324933c867dc6a099850
Velibor Markovic <veljko85@users.noreply.github.com>
2022-11-16 13:27:47 +0100

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -419,6 +419,13 @@ let popUpOpen = false;
 //POP UP//////////////////////////////////////////////////////UNCOMMENT THIS CODE BELOW TO TURN POP UP ON
 // for (const showPopUp of showPopUps) {
 //   showPopUp.addEventListener("click", () => {
+//     if (hamOpened) {
+//       navContainer[0].classList.toggle("fade-in-nav");
+//       hamOpened = false;
+//       icons.forEach((icon) => {
+//         icon.classList.toggle("open");
+//       });
+//     }
 //     popUpBlurBg[0].style.display = "block";
 //     popUp.style.display = "block";
 //     popUpOpen = true;
---
+++ script/textMobile.js
@@ -0,0 +1,155 @@
+if (window.innerHeight > window.innerWidth) {
+  document.getElementsByClassName("text")[0].innerHTML =
+    "We are building the smartest vertical city, featuring" +
+    "<br />" +
+    "cutting-edge real estate technologies.";
+  document.getElementsByClassName("text")[1].innerHTML =
+    "Our mission: Build safer, faster and better. Spark a" +
+    "<br />" +
+    "new era of living, working, and being.";
+  document.getElementsByClassName("text")[2].innerHTML =
+    "We invite startups to work with us." +
+    "<br />" +
+    "Contact us, integrate, and join our mission," +
+    "<br />" +
+    "starting tomorrow.";
+  document.getElementsByClassName("text")[3].innerHTML =
+    "Technology is transforming the way buildings are" + "<br />" + "planed.";
+  document.getElementsByClassName("text")[4].innerHTML =
+    "We're using the BMI method to consolidate data" +
+    "<br />" +
+    "provided by all stake holders during The Vertical's" +
+    "<br />" +
+    "planning and design phases.";
+  document.getElementsByClassName("text")[5].innerHTML =
+    "Working on a cutting-edge service or technology" +
+    "<br />" +
+    "that takes planning, design, and collaboration to the" +
+    "<br />" +
+    "next level? We want to know!";
+  document.getElementsByClassName("text")[6].innerHTML =
+    "New approaches to construction are revolutionizing" +
+    "<br />" +
+    "the building industry.";
+  document.getElementsByClassName("text")[7].innerHTML =
+    "We are looking for technologies in all ConTech" + "<br />" + "segments,";
+  document.getElementsByClassName("text")[8].innerHTML =
+    "Have you developed a construction approach or" +
+    "<br />" +
+    "technology we've never heard of? Tell us today!";
+  document.getElementsByClassName("text")[9].innerHTML =
+    "Smart sensors and IoT have set a new standard for" +
+    "<br />" +
+    "home and work connectivity.";
+  document.getElementsByClassName("text")[10].innerHTML =
+    "The Vertical will be outfitted with a comprehensive" +
+    "<br />" +
+    "Iot and sensor system, registering millions of data" +
+    "<br />" +
+    "points every minute.";
+  document.getElementsByClassName("text")[11].innerHTML =
+    "Developing a revolutionary smart technology?" +
+    "<br />" +
+    "Contact us today!";
+  document.getElementsByClassName("text")[12].innerHTML =
+    "Buildings are becoming more self-sufficient, with" +
+    "<br />" +
+    "reduced operating costs and a lower carbon";
+  document.getElementsByClassName("text")[13].innerHTML =
+    "The data points collected by The Vertical's IoT and" +
+    "<br />" +
+    "sensor system will be harnessed by a cloud-based" +
+    "<br />" +
+    "control AI platform to perform information" +
+    "<br />" +
+    "integration, optimization, and data analysis.";
+  document.getElementsByClassName("text")[14].innerHTML =
+    "Working on innovations in the fields of management," +
+    "<br />" +
+    "maintenance, or optimization? We want to hear from" +
+    "<br />" +
+    "you!";
+  document.getElementsByClassName("text")[15].innerHTML =
+    "Today's clients expect a fast, seamless experience" +
+    "<br />" +
+    "from start to finish.";
+  document.getElementsByClassName("text")[16].innerHTML =
+    "People at The Vertical will be able to digitally book" +
+    "<br />" +
+    "services, search for businesses and goods," +
+    "<br />" +
+    "coordinate deliveries, and more.";
+  document.getElementsByClassName("text")[17].innerHTML =
+    "Developing a technology that revolutionizes the" +
+    "<br />" +
+    "experiance of city living? Contact us today!";
+  document.getElementsByClassName("text")[18].innerHTML =
+    "Sustainable communities are places that people" +
+    "<br />" +
+    "enjoy inhabiting,";
+  document.getElementsByClassName("text")[19].innerHTML =
+    "We aim to have well-designed green spaces to" +
+    "<br />" +
+    "create a true feeling of an ecological park. City" +
+    "<br />" +
+    "residents will look outside their windows,";
+  document.getElementsByClassName("text")[20].innerHTML =
+    "Working on incorporating nature into buildings?" +
+    "<br />" +
+    "Creating new green livable spaces? We want to hear" +
+    "<br />" +
+    "about it. Get in touch!";
+  document.getElementsByClassName("text")[21].innerHTML =
+    "The buildings we construct and the energy they" +
+    "<br />" +
+    "consume have a direct impact on the environment.";
+  document.getElementsByClassName("text")[22].innerHTML =
+    "The Vertical will feature a state-of-the-art Energy" +
+    "<br />" +
+    "Management System (EMS). The EMS will monitor" +
+    "<br />" +
+    "and optimize the building's energy use in real time," +
+    "<br />" +
+    "making adjustments as needed to minimize the" +
+    "<br />" +
+    "carbon footprint.";
+  document.getElementsByClassName("text")[23].innerHTML =
+    "Developing a next-generation technology for energy" +
+    "<br />" +
+    "management, renewable energy, or sustainability?" +
+    "<br />" +
+    "Partner with us!";
+  document.getElementsByClassName("text")[24].innerHTML =
+    "Mobility technologies are reshaping the way people" +
+    "<br />" +
+    "are transported, and innovations in logistics are" +
+    "<br />" +
+    "improving the storage and delivery of goods.";
+  document.getElementsByClassName("text")[25].innerHTML =
+    "Micro mobility solutions, the rise of robotics and" +
+    "<br />" +
+    "smart elevators make it easy to get from point A" +
+    "<br />" +
+    "to point B.";
+  document.getElementsByClassName("text")[26].innerHTML =
+    "Developing a futuristic means of conveying people" +
+    "<br />" +
+    "or storing and delivering goods? We want to know." +
+    "<br />" +
+    "Contact us today!";
+  //
+  document.getElementsByClassName("text")[27].innerHTML =
+    "In today's fast-paced world, it's more important than" +
+    "<br />" +
+    "ever to take care of our mind and bodies.";
+  document.getElementsByClassName("text")[28].innerHTML =
+    "From gyms and yoga studios to community gardens" +
+    "<br />" +
+    "and parks, there will be wellness options for" +
+    "<br />" +
+    "everyone.";
+  document.getElementsByClassName("text")[29].innerHTML =
+    "Developing an innovative fitness, wellness, or" +
+    "<br />" +
+    "community technology? We want you on board!";
+}
